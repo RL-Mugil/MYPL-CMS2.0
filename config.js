d855c82531583f1cdabae1970b9dba0a2a0ea76c
Mugilvannan <mugilvannan@myipstrategy.com>
2026-03-20 19:57:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,31 @@
 window.__APP_CONFIG__ = window.__APP_CONFIG__ || {};
 
 (function initAppConfig() {
-  var host = window.location.hostname || '';
+  var host = (window.location.hostname || '').toLowerCase();
+
   var isLocal = host === 'localhost' || host === '127.0.0.1' || host === '';
-  var isStaging = /staging|preview|dev/i.test(host);
-  var env = isLocal ? 'development' : (isStaging ? 'staging' : 'production');
+  var isGitHubPages = host === 'rl-mugil.github.io';
+  var isCloudflarePages = host.indexOf('.pages.dev') > -1;
+  var isStaging = /staging|preview|dev/i.test(host) && !isCloudflarePages;
+
+  var env = 'production';
+  if (isLocal) env = 'development';
+  else if (isStaging) env = 'staging';
+
+  var sharedApiBase = 'https://mypl-new.mugilvannan.workers.dev';
+  var sharedClerkKey = 'pk_test_c2V0dGxlZC10b3VjYW4tNTguY2xlcmsuYWNjb3VudHMuZGV2JA';
 
   var defaults = {
     environment: env,
     apiBaseByEnv: {
-      development: 'https://mypl-new.mugilvannan.workers.dev',
-      staging: '',
-      production: 'https://mypl-new.mugilvannan.workers.dev'
+      development: sharedApiBase,
+      staging: sharedApiBase,
+      production: sharedApiBase
     },
     clerkPublishableKeyByEnv: {
-      development: 'pk_test_c2V0dGxlZC10b3VjYW4tNTguY2xlcmsuYWNjb3VudHMuZGV2JA',
-      staging: '',
-      production: 'pk_test_c2V0dGxlZC10b3VjYW4tNTguY2xlcmsuYWNjb3VudHMuZGV2JA'
+      development: sharedClerkKey,
+      staging: sharedClerkKey,
+      production: sharedClerkKey
     },
     errorEndpointByEnv: {
       development: '',
@@ -29,9 +38,13 @@ window.__APP_CONFIG__ = window.__APP_CONFIG__ || {};
   var userConfig = window.__APP_CONFIG__;
   var merged = Object.assign({}, defaults, userConfig);
 
-  merged.apiBase = userConfig.apiBase || merged.apiBaseByEnv[env] || '';
-  merged.clerkPublishableKey = userConfig.clerkPublishableKey || merged.clerkPublishableKeyByEnv[env] || '';
-  merged.errorEndpoint = userConfig.errorEndpoint || merged.errorEndpointByEnv[env] || '';
+  if (isGitHubPages || isCloudflarePages) {
+    merged.environment = 'production';
+  }
+
+  merged.apiBase = userConfig.apiBase || merged.apiBaseByEnv[merged.environment] || '';
+  merged.clerkPublishableKey = userConfig.clerkPublishableKey || merged.clerkPublishableKeyByEnv[merged.environment] || '';
+  merged.errorEndpoint = userConfig.errorEndpoint || merged.errorEndpointByEnv[merged.environment] || '';
 
   window.APP_CONFIG = merged;
 })();
