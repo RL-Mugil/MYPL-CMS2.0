41a98b1650764efb06d1f8be9efddf3b76ad3be3
Mugilvannan <mugilvannan@myipstrategy.com>
2026-03-21 08:23:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,28 @@
 window.__APP_CONFIG__ = window.__APP_CONFIG__ || {};
 
 (function initAppConfig() {
-  var host = window.location.hostname || '';
+  var host = (window.location.hostname || '').toLowerCase();
+
   var isLocal = host === 'localhost' || host === '127.0.0.1' || host === '';
-  var isStaging = /staging|preview|dev/i.test(host);
-  var env = isLocal ? 'development' : (isStaging ? 'staging' : 'production');
+  var isGitHubPages = host === 'rl-mugil.github.io';
+  var isCloudflarePages = host.indexOf('.pages.dev') > -1;
+  var isCustomProd = host === 'cms.metayage.com';
+  var isStaging = /staging|preview/i.test(host);
+
+  var env = 'production';
+  if (isLocal) env = 'development';
+  else if (isStaging && !isGitHubPages && !isCloudflarePages && !isCustomProd) env = 'staging';
 
   var defaults = {
     environment: env,
     apiBaseByEnv: {
       development: 'https://mypl-new.mugilvannan.workers.dev',
-      staging: '',
+      staging: 'https://mypl-new.mugilvannan.workers.dev',
       production: 'https://mypl-new.mugilvannan.workers.dev'
     },
     clerkPublishableKeyByEnv: {
       development: 'pk_test_c2V0dGxlZC10b3VjYW4tNTguY2xlcmsuYWNjb3VudHMuZGV2JA',
-      staging: '',
+      staging: 'pk_test_c2V0dGxlZC10b3VjYW4tNTguY2xlcmsuYWNjb3VudHMuZGV2JA',
       production: 'pk_test_c2V0dGxlZC10b3VjYW4tNTguY2xlcmsuYWNjb3VudHMuZGV2JA'
     },
     streamEnabledByEnv: {
@@ -41,7 +48,9 @@ window.__APP_CONFIG__ = window.__APP_CONFIG__ || {};
 
   merged.apiBase = userConfig.apiBase || merged.apiBaseByEnv[env] || '';
   merged.clerkPublishableKey = userConfig.clerkPublishableKey || merged.clerkPublishableKeyByEnv[env] || '';
-  merged.streamEnabled = typeof userConfig.streamEnabled === 'boolean' ? userConfig.streamEnabled : !!merged.streamEnabledByEnv[env];
+  merged.streamEnabled = typeof userConfig.streamEnabled === 'boolean'
+    ? userConfig.streamEnabled
+    : !!merged.streamEnabledByEnv[env];
   merged.streamApiKey = userConfig.streamApiKey || merged.streamApiKeyByEnv[env] || '';
   merged.errorEndpoint = userConfig.errorEndpoint || merged.errorEndpointByEnv[env] || '';
 
